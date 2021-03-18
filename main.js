65608fb268e9ce198cd0f3d486e7dfae0346f604
SamSwaroop <samswaroop97@msitprogram.net>
2021-03-18 23:54:40 +0530

    version2

---
+++ main.js
@@ -0,0 +1,25 @@
+var pageCounter=1
+var animalContainer = document.getElementById("animal-info")
+var btn = document.getElementById("btn")
+btn.addEventListener('click',function(){
+    var HttpRequest = new XMLHttpRequest;
+    HttpRequest.open('GET','https://learnwebcode.github.io/json-example/animals-'+pageCounter+'.json')
+    HttpRequest.onload = function(){
+    var Data = JSON.parse(HttpRequest.responseText)
+    renderHTML(Data)
+    }
+    HttpRequest.send()
+    pageCounter++
+    if(pageCounter > 3){
+        btn.classList.add("hide-me")
+    }
+})
+
+function renderHTML(data){
+    var htmlString=""
+    for(i=0;i<data.length;i++){
+        htmlString += "<p>" +data[i].name+ " is a " +data[i].species+ "</p>"
+    }
+     animalContainer.insertAdjacentHTML('beforeend',htmlString)
+
+}
